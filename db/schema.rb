499f27dce7635bec90bff0591d78332f59208f7a
Felipe Roa <felipeyosmar@gmail.com>
2022-09-28 16:50:21 -0500

    add bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_172541) do
+ActiveRecord::Schema.define(version: 2022_09_28_201324) do
 
   create_table "articles", charset: "latin1", force: :cascade do |t|
     t.string "title"
@@ -52,7 +52,12 @@ ActiveRecord::Schema.define(version: 2022_05_05_172541) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "wish_lists", charset: "latin1", force: :cascade do |t|
