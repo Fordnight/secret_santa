c87ad8d4f2273b14882d035c4e9529771312d91b
Felipe Roa <felipeyosmar@gmail.com>
2022-10-14 20:38:22 +0000

    modified list.rb

--- app/models/list.rb
+++ app/models/list.rb
@@ -19,21 +19,27 @@ class List < ApplicationRecord
 
   def run_raffle
     assigned = []
-    available_players = []
 
     players.each do |player|
-      available_players = players.collect(&:id) - [player.id] - assigned
-
-      if available_players.empty?
-        interchangable_user = PlayerList.find(players.first.id)
-        available_players.push interchangable_user.secret_friend_id
-        interchangable_user.update(secret_friend_id: player.id)
-      end
-
-      secret_friend = available_players.sample
+      secret_friend = available_players(player, assigned).sample
       assigned.push(secret_friend)
-      player_raffle = self.player_lists.find_by_player_id(player.id)
+      player_raffle = player_lists.find_by_player_id(player.id)
       player_raffle.update(secret_friend_id: secret_friend)
     end
+  end
+
+private
+  def interchange_player(player)
+    interchangable_user = player_lists.find_by_player_id(players.first.id)
+    available_user = interchangable_user.secret_friend_id
+    interchangable_user.update(secret_friend_id: player.id)
+    available_user
+  end
+
+  def available_players(player, assigned)
+      non_assigned = players.collect { |p| p.id }
+      non_assigned -= [player.id] + assigned
+      non_assigned.push interchange_player(player) if non_assigned.empty?
+      non_assigned
   end
 end
